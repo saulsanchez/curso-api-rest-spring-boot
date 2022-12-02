a26fc7f8e307315bbc5c87c6ac79d1b3c6467ec9
saulsanchez <bluemonday.saul@gmail.com>
2022-12-02 12:46:29 +0100

    Probando los repositorios: Configuración.
    Configuración de tests unitarios con BD en memoria H2

---
+++ src/test/java/com/saulsanchez/universidad/universidadbackend/repositorios/CarreraRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.saulsanchez.universidad.universidadbackend.repositorios;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarreraRepositoryTest {
+
+    @Test
+    void findCarrerasByNombreContains() {
+    }
+
+    @Test
+    void findCarrerasByNombreContainsIgnoreCase() {
+    }
+
+    @Test
+    void findCarrerasByCantidadAniosAfter() {
+    }
+}
