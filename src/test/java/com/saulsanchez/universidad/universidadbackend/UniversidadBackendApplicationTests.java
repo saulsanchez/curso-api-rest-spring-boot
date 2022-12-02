80c2ea667b966c103f7a79667fd0599397fa09fd
saulsanchez <bluemonday.saul@gmail.com>
2022-12-02 11:24:54 +0100

    Escribiendo el primer test unitario

--- src/test/java/com/saulsanchez/universidad/universidadbackend/UniversidadBackendApplicationTests.java
+++ src/test/java/com/saulsanchez/universidad/universidadbackend/UniversidadBackendApplicationTests.java
@@ -1,13 +1,42 @@
 package com.saulsanchez.universidad.universidadbackend;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class UniversidadBackendApplicationTests {
 
+	Calculadora calculadora = new Calculadora();
+
 	@Test
-	void contextLoads() {
+	@DisplayName("Suma de valor A y valor B")
+	void sumaDeValores() {
+		//given
+		int valorA = 3;
+		int valorB = 3;
+
+		//when
+		int expectativa = calculadora.sumar(valorA, valorB);
+
+		//then
+		int resultadoEsperado = 5;
+		assertThat(expectativa).isEqualTo(resultadoEsperado);
+	}
+
+	@Test
+	@DisplayName("Test deprecado")
+	@Disabled("Test deprecado")
+	void testDesactivado() {
+
+	}
+
+	class Calculadora {
+		int sumar(int valor_a, int valor_b) {
+			return valor_a + valor_b;
+		}
 	}
 
 }
